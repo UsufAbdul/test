81c323558c0a2ebdade3145d893d707e7c5ce305
unknown <yusuf Abdulkadir>
2024-08-06 17:14:03 +0100

    working more on async and promise functions

--- script2.js
+++ script2.js
@@ -1,7 +1,7 @@
 /*
 const fetch = require("node-fetch");
 
-const getPeople = (fetch) => {
+const getPeoplePromise = (fetch) => {
   return fetch("https://swapi.dev/api/people")
     .then((Response) => Response.json())
     .then((data) => {
@@ -13,8 +13,28 @@ const getPeople = (fetch) => {
     });
 };
 
+//getPeoplePromise(fetch);
+
+//const fetch = require("node-fetch");
+
+const getPeople = async (fetch) => {
+  const getRequest = await fetch("https://swapi.dev/api/people");
+  const data = getRequest.json();
+  console.log(data);
+  return {
+    count: data.count,
+    results: data.results,
+  };
+};
+
 getPeople(fetch);
+
+module.exports = {
+  getPeoplePromise,
+  getPeople,
+};
 */
+/*
 //SECOND WAY OF WRITING THE FUNCTION USING PROMISE
 
 const fetch = (...args) =>
@@ -48,7 +68,40 @@ const getPeople = async (fetch) => {
   };
 };
 //getPeople(fetch);
+*/
 
+const fetch = require("node-fetch");
+
+// Function using promises
+const getPeoplePromise = (fetch) => {
+  return fetch("https://swapi.dev/api/people")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      return {
+        count: data.count,
+        results: data.results,
+      };
+    });
+};
+
+// Function using async/await
+const getPeople = async (fetch) => {
+  try {
+    const response = await fetch("https://swapi.dev/api/people");
+    const data = await response.json(); // Await here as well
+    console.log(data);
+    return {
+      count: data.count,
+      results: data.results,
+    };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error; // Re-throw error to handle it in the calling function
+  }
+};
+
+// Export both functions
 module.exports = {
   getPeoplePromise,
   getPeople,
--- script2.test.js
+++ script2.test.js
@@ -1,22 +1,15 @@
-/*
-const fetch = (...args) =>
-  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+const { getPeoplePromise, getPeople } = require("./script2");
 
 const swapi = require("./script2");
 
 it("calls swapi to get people", () => {
   swapi.getPeople(fetch).then((data) => {
-    expect(data.count).toEqual(87);
+    expect(data.count).toEqual(82);
   });
 });
-*/
 
-const fetch = (...args) =>
-  import("node-fetch").then(({ default: fetch }) => fetch(...args));
-
-const { getPeople } = require("./script2"); // Adjust this line if using ES modules
-
-it("calls swapi to get people", async () => {
-  const data = await getPeople(fetch);
-  expect(data.count).toEqual(82);
+it("calls swapi to get people with a promise", () => {
+  swapi.getPeoplePromise(fetch).then((data) => {
+    expect(data.count).toEqual(82);
+  });
 });
